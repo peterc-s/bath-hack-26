a9e4c423c91903b652eb4968edfa55828364ead4
peterc-s <petercs@purelymail.com>
2026-03-28 20:49:47 +0000

    Start some basic programming, give up on serial for now

--- trickle-down-dragonslayer/src/basic/main.bas
+++ trickle-down-dragonslayer/src/basic/main.bas
@@ -1,2 +1,31 @@
-<HELLO> PRINT "HELLO, WORLD!"
-GOTO @HELLO
+REM **************************************************************************** 
+REM                        TRICKLE-DOWN DRAGONSLAYER
+REM                           PETER CHAPLIN-SMITH
+REM                           LOUIS LEAR-BELCHER
+REM                             HEATH MITCHELL
+REM **************************************************************************** 
+
+<START> GOSUB @BORDER
+<LOOP> PRINT "HELLO";
+GOTO @LOOP
+END
+
+<BORDER> POKE 35,255
+PRINT "########################################";
+FOR I = 1 TO 22 : PRINT "#                                      #"; : NEXT I
+PRINT "########################################";
+
+REM LEFT
+POKE 32,1
+REM WIDTH
+POKE 33,38
+REM TOP
+POKE 34,1
+REM BOTTOM
+POKE 35,23
+
+REM CLEAR
+CALL -936
+HOME
+
+RETURN
--- trickle-down-dragonslayer/src/trickle_down_dragonslayer/__init__.py
+++ trickle-down-dragonslayer/src/trickle_down_dragonslayer/__init__.py
@@ -1,14 +1,25 @@
 import serial
 import time
 
-PORT = "/dev/ttyACM0"
-BAUD_RATE = 9600
+PORT = "/dev/ttyUSB0"
+BAUD_RATE = 300
 
 
 def main():
     try:
-        with serial.Serial(PORT, BAUD_RATE, timeout=2) as ser:
+        with serial.Serial(
+            PORT,
+            BAUD_RATE,
+            timeout=2,
+            parity=serial.PARITY_NONE,
+            bytesize=serial.EIGHTBITS,
+            xonxoff=False,
+            rtscts=False,
+            dsrdtr=False,
+            stopbits=1,
+        ) as ser:
             print("Waiting 2 seconds for connection...")
+            ser.dtr = True
             time.sleep(2)
 
             print("Resetting buffers")
@@ -16,14 +27,14 @@ def main():
             ser.reset_output_buffer()
 
             print("Sending: ping")
-            ser.write(b"ping\n")
-            raw_response = ser.readline().decode("utf-8", errors="ignore")
-            clean_response = raw_response.strip().replace("\x00", "")
-
-            if "pong" in clean_response:
-                print(f"Success! Received: {clean_response}")
-            else:
-                print(f"Unexpected data: {repr(raw_response)}")
+            ser.write(b"ping\r\n")
+            # raw_response = ser.readline().decode("utf-8", errors="ignore")
+            # clean_response = raw_response.strip().replace("\x00", "")
+            #
+            # if "pong" in clean_response:
+            #     print(f"Success! Received: {clean_response}")
+            # else:
+            #     print(f"Unexpected data: {repr(raw_response)}")
 
     except Exception as e:
         print(f"Error: {e}")
