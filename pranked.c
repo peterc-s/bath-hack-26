23aa479f3477190531b90dd0eef8813135eba170
peterc-s <petercs@purelymail.com>
2026-03-29 07:27:29 +0100

    add prank

---
+++ pranked.c
@@ -0,0 +1,33 @@
+#include <conio.h>
+#include <stddef.h>
+#include <stdio.h>
+
+int main(void) {
+  clrscr();
+  printf("****************************************");
+  printf("* http://devpost.com/                  *");
+  printf("****************************************");
+  printf("*                                      *");
+  printf("* Offline Mode                         *");
+  printf("* ------------                         *");
+  printf("*                                      *");
+  printf("* Firefox is currently in offline      *");
+  printf("* mode and can't browse the web        *");
+  printf("*                                      *");
+  printf("* ------------                         *");
+  printf("*                                      *");
+  printf("* The browser is operating in offline  *");
+  printf("* and cannot connect to the requested  *");
+  printf("* item.                                *");
+  printf("*                                      *");
+  printf("* - Is the computer connected to an    *");
+  printf("*   active network?                    *");
+  printf("* - Place the browser in online mode   *");
+  printf("*   and try again.                     *");
+  printf("*                                      *");
+  printf("*                                      *");
+  printf("****************************************");
+  getchar();
+
+  return 0;
+}
