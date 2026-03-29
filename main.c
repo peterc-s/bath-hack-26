f62eb002af27c5c553c27bf8aef0f785b9c218f0
peterc-s <petercs@purelymail.com>
2026-03-29 04:28:22 +0100

    prompting sort of working

--- main.c
+++ main.c
@@ -15,6 +15,22 @@ typedef enum {
   WEAPON_GUN,
 } Weapon;
 
+typedef enum {
+  ROOM_WHITE_CASTLE,
+  ROOM_OUTSKIRTS_LONDIS,
+  ROOM_JOHNNY_FARM,
+  ROOM_JOHNNY_FARMHOUSE,
+  ROOM_FERRY_PIER,
+  ROOM_BEACH,
+  ROOM_CAR_FACTORY,
+  ROOM_MERLIN_WALL,
+  ROOM_IRON_WALL,
+  ROOM_MOUNT_GREMLIN,
+} Room;
+
+#define NUM_VERBS 5
+const char *VERBS[] = {"ATTACK", "GOTO", "PICKUP", "LOOK", "USE"};
+
 typedef struct {
   byte health;
   byte damage;
@@ -25,6 +41,7 @@ typedef struct {
   byte health;
   byte damage;
   byte weapon;
+  Room room;
   char name[MAXLINELEN];
   char weapon_name[MAXLINELEN];
 } Player;
@@ -58,6 +75,7 @@ void readline(char *instr) {
 void init_player() {
   player.health = 4;
   player.damage = 1;
+  player.room = ROOM_WHITE_CASTLE;
   player.weapon = WEAPON_SWORD;
   strcpy(player.weapon_name, "SWORD");
 }
@@ -108,9 +126,48 @@ void intro() {
   cgetc();
 }
 
+void do_attack(void) { printf("\nYOU SWING YOUR %s!", player.weapon_name); }
+void do_goto(void) { printf("\nWHERE TO?"); }
+void do_pickup(void) { printf("\nYOU PICKED IT UP."); }
+void do_look(void) { printf("\nYOU SEE NOTHING BUT DEBT."); }
+void do_use(void) { printf("\nYOU USED IT."); }
+
+void (*verb_functions[])(void) = {do_attack, do_goto, do_pickup, do_look,
+                                  do_use};
+
+void prompt_action() {
+  char *verb_part;
+  char *noun_part;
+  byte i;
+  int found = 0;
+
+  while (!found) {
+    printf("\n\nWHAT WILL YOU DO? ");
+    readline(input);
+
+    verb_part = input;
+    noun_part = strchr(input, ' ');
+    if (noun_part != NULL) {
+      *noun_part = '\0'; // Terminate the verb string at the space
+      noun_part++;       // Move pointer to the start of the noun
+    }
+    for (i = 0; i < NUM_VERBS; ++i) {
+      if (strcmp(verb_part, VERBS[i]) == 0) {
+        verb_functions[i](); // Call the corresponding function
+        found = 1;
+        break;
+      }
+    }
+    if (!found) {
+      printf("\nI DON'T UNDERSTAND '%s'.", verb_part);
+    }
+  }
+}
+
 int main(void) {
   init_player();
   intro();
-  printf("%s", player.name);
+  prompt_action();
+  cgetc();
   return 0;
 }
